e039d13de725e325ed9add0b47950fa00e5e3003
aaboud10 <abdallah.aboud@torontomu.ca>
2024-11-13 18:50:34 -0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const dbOperations = require('./dbOperations');
 
 const app = express();
-app.use(cors());           // Enable Cross-Origin Resource Sharing
-app.use(express.json());    // Parse JSON bodies
+app.use(cors());        
+app.use(express.json());   
 
 // --------- User Routes ---------
 
